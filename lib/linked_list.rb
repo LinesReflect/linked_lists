24846fac4444c7e998be795cc436468b533d3040
Reflection <reflectionoflines@gmail.com>
2023-02-23 11:57:58 -0600

    Create append method.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,4 +4,8 @@ class LinkedList
   def initialize()
     @head = nil
   end
+
+  def append(value)
+    @head ? [@head = Node.new(value), @head.next_node = @head] : @head = Node.new(value)
+  end
 end
