0bf18b9a12bde87e2a69af31edd3b35183e0a3d9
Reflection <reflectionoflines@gmail.com>
2023-02-23 12:42:20 -0600

    Refactor append method.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,11 +1,25 @@
 require_relative 'node'
 
 class LinkedList
+  attr_accessor :head
+  
   def initialize()
     @head = nil
   end
 
   def append(value)
+    if @head.nil? 
+      @head = Node.new(value) 
+    else
+      last_node = @head
+      while(!last_node.next_node.nil?)
+        last_node = last_node.next_node
+      end
+      last_node.next_node = Node.new(value)
+    end
+  end
+
+  def prepend(value)
     @head ? [@head = Node.new(value), @head.next_node = @head] : @head = Node.new(value)
   end
 end
